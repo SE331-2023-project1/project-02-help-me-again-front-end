60db54e9dd522894efd7cbc5e91bf06c129b969f
thanap151255 <thanap151255@gmail.com>
2023-10-24 05:56:56 +0700

    final in final

--- src/views/announcement/AnnouncementView.vue
+++ src/views/announcement/AnnouncementView.vue
@@ -51,98 +51,46 @@ export default {
 // const totalAnnouncement = ref<number>(10)
 </script>
 
-<!-- <template>
-    <main class="flex flex-col items-center justify-center">
-        <div
-            class="mt-5 font-pri flex items-center justify-center p-3 w-3/4 sm:w-2/4 h-4/5 text-2xl font-bold text-gray-900  bg-white border border-gray-300 rounded-lg shadow-md">
-            Announcement
-        </div>
-
-
-        <div
-            class="mt-5 mb-10 font-pri flex flex-col items-left justify-left p-3 w-3/4 sm:w-2/4 h-auto text-xl font-bold text-gray-900 bg-white border border-gray-300 rounded-lg shadow-md">
-            <div class="flex items-center space-x-3">
-                <img src="src\assets\help.png" alt="Person's Photo" class="w-20 h-20 rounded-full">
-                <div>
-                    <p class="font-pri font-semibold text-md">Dreygo</p>
-                    <p class="font-pri text-sm font-semibold text-gray-600">Date: December 2, 2023</p>
-                    <p class="font-pri text-sm font-semibold text-gray-600">Time: 05:12 AM</p>
-                </div>
-            </div>
-            
-            <div class="mt-5 flex flex-col pl-3">
-                <p class="font-pri font-semibold text-md">December Event</p>
-                <p class="font-pri text-md text-gray-600">This mount we have big event for our student please prepare yourself</p>
-            </div>
-            
-            <div class="mt-5  relative">
-                <img :src="photos[selectedPhoto]" class="object-cover w-full h-60 md:h-96 rounded-lg"
-                    alt="Selected Photo" />
-                <div
-                    class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
-                    <span
-                        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
-                        @click="prevPhoto">
-                        <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 1 1 5l4 4" />
-                        </svg>
-                        <span class="sr-only">Previous</span>
-                    </span>
-                </div>
-                <div
-                    class="absolute top-0 right-0  flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
-                    <span
-                        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
-                        @click="nextPhoto">
-                        <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span class="sr-only">Next</span>
-                    </span>
-                </div>
-            </div>
-        </div>
-
-    </main>
-</template> -->
   
 <template>
-    <main class="flex flex-col items-center justify-center">
-      <div class="mt-5 font-pri w-full sm:w-3/4">
-        <div class="bg-white border border-gray-300 rounded-lg shadow-md p-3">
-          <h1 class="text-2xl font-bold text-gray-900 mb-5">Announcement</h1>
-  
-          <div class="flex items-center space-x-3">
-            <img src="src/assets/help.png" alt="Person's Photo" class="w-20 h-20 rounded-full">
-            <div>
-              <p class="font-pri font-semibold text-gray-900 text-md">Dreygo</p>
-              <p class="font-pri text-sm font-semibold text-gray-600">Date: December 2, 2023</p>
-              <p class="font-pri text-sm font-semibold text-gray-600">Time: 05:12 AM</p>
+    <main class="p-6 min-h-screen font-pri">
+      <div class="max-w-screen-lg mx-auto">
+        <div class="bg-white rounded-lg shadow-md p-6">
+          <div class="flex items-center mt-4 mb-4">
+            <!-- <img src="src/assets/help.png" alt="Person's Photo" class="w-20 h-20 rounded-full"> -->
+            <div class="mx-auto text-center">
+              <p class="text-2xl font-semibold text-gray-900">Dreygo</p>
+              <p class="text-sm text-gray-600">Date: December 2, 2023</p>
+              <p class="text-sm text-gray-600">Time: 05:12 AM</p>
             </div>
           </div>
-  
-          <div class="mt-5">
-            <h2 class="text-xl font-bold text-gray-900 font-pri">December Event</h2>
-            <p class="text-md text-gray-600">This month we have a big event for our students. Please prepare yourself.</p>
+      </div>
+
+      <div class="bg-white rounded-lg mt-4 shadow-md p-6">
+        <div class="flex items-center mb-4">
+            <h1 class="text-3xl font-bold text-gray-900 mb-4">December Event</h1>
+          <p class="text-lg text-gray-700">
+            This month we have a big event for our students. Please prepare yourself.
+          </p>
           </div>
-  
-          <div class="mt-5 relative">
+      </div>
+
+
+      <div class="bg-white rounded-lg mt-4 shadow-md p-6">
+        <div class="flex items-center ">
+            <div class=" relative mx-auto">
             <img :src="photos[selectedPhoto]" class="object-cover w-full h-60 md:h-96 rounded-lg" alt="Selected Photo" />
-            <div class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
+            <div class="absolute top-0 left-0 z-10 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
               <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none" @click="prevPhoto">
-                <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <svg class="w-6 h-6 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
                 </svg>
                 <span class="sr-only">Previous</span>
               </span>
             </div>
-            <div class="absolute top-0 right-0 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
+            <div class="absolute top-0 right-0 z-10 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none">
               <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none" @click="nextPhoto">
-                <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <svg class="w-6 h-6 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                 </svg>
                 <span class="sr-only">Next</span>
@@ -150,7 +98,13 @@ export default {
             </div>
           </div>
         </div>
+          </div>
       </div>
+
+      
+      
+      
     </main>
   </template>
+  
   
