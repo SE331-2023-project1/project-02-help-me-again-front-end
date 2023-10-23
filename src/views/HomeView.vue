95826cfca9686ef647c9b853eadd78a97172d694
thanap151255 <thanap151255@gmail.com>
2023-10-24 00:00:37 +0700

    member jaa

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,39 +7,27 @@
     <h1 class="font-fig text-3xl font-bold  px-4 py-2 text-center">SE331 TERM PROJECT</h1>
     <p class="font-mono text-center">Component-Based Software Development</p>
     <br>
-    <p class="font-fig font-bold text-base ">Group Name: E-Mam_BodyMom</p>
+    <p class="font-fig font-bold text-base ">Group Name: Help Me PLEASE</p>
     <h2 class="font-fig my-2 font-bold">Team Members</h2>
     <div class="grid lg:grid-cols-3 md:grid-cols-1 gap-24 gap-y-12 mt-10">
       <div class="member flex flex-col items-center justify-center">
-        <img src="https://cdn.discordapp.com/attachments/715915099176566867/1143188492550156338/367401485_1654967945002581_6150912557020411318_n.jpg" alt="" class="animate-fade-down h-48 rounded-full mb-4 shadow-md -z-10" />
-        
-        <p><span class="font-fig font-bold">Name:</span> Thiwakon Sakunchao</p>
-        <p><span class="font-fig font-bold">Student ID:</span> 642115020</p>
-        <div class="flex items-center">
-          <a href="https://github.com/thiwakonsakunchao" target="_blank"
-            class="rounded-lg bg-dark text-light text-lg font-semibold mt-4 flex items-center">
-            <img src="../assets/github.png" class="font-fig h-5 w-5 mr-2">Github</a>
-        </div>
+        <!-- pic -->
+        <img src="../assets/S1.jpg" alt="" class="animate-fade-down h-48 rounded mb-4 shadow-md -z-10" />
+        <p><span class="font-fig font-bold">Name:</span> Tanapong Yamkamol</p>
+        <p><span class="font-fig font-bold">Student ID:</span> 642115022</p>
       </div>
       <div class="member flex flex-col items-center justify-center">
-        <img src="https://cdn.discordapp.com/attachments/715915099176566867/1143188468126720010/021.JPG" alt="" class="animate-fade-down h-48 rounded-full mb-4 shadow-md -z-10" />
-        <p><span class="font-fig font-bold">Name:</span> Thanakorn Waleejaroenpong</p>
-        <p><span class="font-fig font-bold">Student ID:</span> 642115021</p>
-        <div class="flex items-center">
-          <a href="https://github.com/Nongpepenasi" target="_blank"
-            class="rounded-lg bg-dark text-light text-lg font-semibold mt-4 flex items-center">
-            <img src="../assets/github.png" class="font-fig h-5 w-5 mr-2">Github</a>
-        </div>
+        <!-- pic -->
+        <img src="../assets/N.jpg" alt="" class="animate-fade-down h-48 rounded mb-4 shadow-md -z-10" />
+        <p><span class="font-fig font-bold">Name:</span> Sasipha Thonkhan</p>
+        <p><span class="font-fig font-bold">Student ID:</span> 642115042</p>
       </div>
       <div class="member flex flex-col items-center justify-center">
-        <img src="https://cdn.discordapp.com/attachments/715915099176566867/1143188511709736970/366105722_7252872991406159_8939141166619055441_n.jpg" alt="" class="animate-fade-down w-48 h-48 rounded-full mb-4 shadow-md" />
-        <p><span class="font-fig font-bold">Name:</span> Phacharanan Thumjaikul</p>
-        <p><span class="font-fig font-bold">Student ID:</span> 642115026</p>
-        <div class="flex items-center">
-          <a href="https://github.com/MMMMMMMPCRN" target="_blank"
-            class="rounded-lg bg-dark text-light text-lg font-semibold mt-4 flex items-center">
-            <img src="../assets/github.png" class="font-fig h-5 w-5 mr-2">Github</a>
-        </div>
+        <!-- pic -->
+        <img src="../assets/BB.jpg" alt="" class="animate-fade-down w-48 h-48 rounded mb-4 shadow-md" />
+        <p><span class="font-fig font-bold">Name:</span> Siriyakorn Chomchuen</p>
+        <p><span class="font-fig font-bold">Student ID:</span> 642115047</p>
+
       </div>
     </div>
   </main>
