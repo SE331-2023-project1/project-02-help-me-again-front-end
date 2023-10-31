01d57989beecc0bf90aeb497a35b6b4d19cd2ec7
NameSasipha <sasipha_th@cmu.ac.th>
2023-10-31 17:28:54 +0700

    create input component and put index

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,6 +13,10 @@ import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import AnnouncementView from '../views/announcement/AnnouncementView.vue'
 import StudentProfile from '../views/student/StudentProfile.vue'
+import AdvisorProfile from '../views/advisor/AdvisorProfile.vue'
+import CreateAnnouncement from '../views/announcement/CreateAnnouncement.vue'
+import StudentProfileAdmin from '../views/student/StudentProfileAdmin.vue'
+import AdvisorProfileAdmin from '../views/advisor/AdvisorProfileAdmin.vue'
 import NProgress from 'nprogress'
 import StudentService from '@/services/StudentService'
 import AdvisorService from '@/services/AdvisorService'
@@ -23,9 +27,10 @@ import { commentStudent } from '@/stores/comment'
 import { commentStudentId } from '@/stores/comment_id'
 import { storeToRefs } from 'pinia'
 import { useAuthStore } from '@/stores/auth.ts'
+import { useAnnouncementStore } from '@/stores/announcement'
 
 import AddPerson from '../views/AddPerson.vue';
-
+  
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -45,9 +50,31 @@ const router = createRouter({
       component: AnnouncementView
     },
     {
+      path: '/createpost',
+      name: 'create-announcement',
+      component: CreateAnnouncement
+    },
+    {
       path: '/studentprofile',
       name: 'studentprofile-view',
-      component: StudentProfile
+      component: StudentProfile,
+    },
+    {
+      path: '/studentprofile-admin/:id',
+      name: 'studentprofile-admin',
+      component: StudentProfileAdmin,
+      props: true
+    },
+    {
+      path: '/advisorprofile',
+      name: 'teaacherprofile-view',
+      component: AdvisorProfile
+    },
+    {
+      path: '/advisorprofile-admin/:id',
+      name: 'advisorprofile-admin',
+      component: AdvisorProfileAdmin,
+      props: true
     },
     {
       path: '/register',
@@ -58,13 +85,13 @@ const router = createRouter({
       path: '/students',
       name: 'student-list',
       component: StudentList,
-      props: (route) => ({ page: parseInt(route.query?.page as string || '1'), limit: parseInt(route.query?.limit as string || '2') })
+      props : (route) => ({page: parseInt(route.query?.page as string || '1'),limit: parseInt(route.query?.limit as string || '2')})
     },
     {
       path: '/advisors',
       name: 'advisor-list',
       component: AdvisorList,
-      props: (route) => ({ page: parseInt(route.query?.page as string || '1'), limit: parseInt(route.query?.limit as string || '2') })
+      props : (route) => ({page: parseInt(route.query?.page as string || '1'),limit: parseInt(route.query?.limit as string || '2')})
     },
     {
       path: '/advisors/:id',
@@ -76,45 +103,45 @@ const router = createRouter({
         const advisorStore = useAdvisorStore()
         const studentStore = useStudentStore()
         try {
-          // const advisor = await advisorStore.getadvisorById(id)
-          // advisorStore.setadvisor(advisor)
-          // const studentPromises = advisor.studentsId.map(async studentId => {
+          // const advisor = await advisorStore.getAdvisorById(id)
+          // advisorStore.setAdvisor(advisor)
+          // const studentPromises = v.studentsId.map(async studentId => {
           //   const student = await studentStore.getStudentById(studentId)
           //   return student
           // });
           // const students = await Promise.all(studentPromises)
           // // console.log(studentPromises)
           // studentStore.setStudent(students.filter(student => student !== null) as StudentItem[]);
-          useAdvisorStore().getAdvisorrById(id)
+          useAdvisorStore().getAdvisorById(id)
           // console.log(studentStore)
-        } catch (error: any) {
-          if (error.response && error.response.status === 404) {
-            // const errorUrl = error.response.config.url;
-            // const resource = errorUrl.includes('advisors') ? 'advisors' : 'students';
+      } catch (error: any) {
+        if (error.response && error.response.status === 404) {
+          // const errorUrl = error.response.config.url;
+          // const resource = errorUrl.includes('advisors') ? 'advisors' : 'students';
 
-            // console.log(`Resource '${resource}' not found.`);
-            // console.log("Resource not found")
+          // console.log(`Resource '${resource}' not found.`);
+          // console.log("Resource not found")
+  
+          const errorUrl = error.response.config.url
+          console.log("Error URL:", errorUrl)
+      
+          const resourceIdentifier = errorUrl.split('/').pop() // Get the last segment
+          console.log("Resource Identifier:", resourceIdentifier)
+      
+          const isAdvisor = resourceIdentifier.charAt(0) === 'T' // Check the first character
+          console.log("Is Advisor:", isAdvisor)
+      
+          const resource = isAdvisor ? 'advisor' : 'student'
+          console.log("Resource:", resource)
 
-            const errorUrl = error.response.config.url
-            console.log("Error URL:", errorUrl)
-
-            const resourceIdentifier = errorUrl.split('/').pop() // Get the last segment
-            console.log("Resource Identifier:", resourceIdentifier)
-
-            const isAdvisor = resourceIdentifier.charAt(0) === 'T' // Check the first character
-            console.log("Is Advisor:", isAdvisor)
-
-            const resource = isAdvisor ? 'advisor' : 'student'
-            console.log("Resource:", resource)
-
-            router.push({
-              name: '404-resource',
-              params: { resource }
-            })
-          } else {
-            router.push({ name: 'network-error' })
-          }
+          router.push({
+            name: '404-resource',
+            params: { resource }
+        })
+        } else {
+          router.push({ name: 'network-error'})
         }
+      }
       },
       children: [
         {
@@ -137,10 +164,10 @@ const router = createRouter({
           // const student = await studentStore.getStudentById(id)
           // studentStore.setStudent(student)
           // const advisor = await advisorStore.getAdvisorById(student.advisorID)
-          // advisorStore.setadvisorr(advisor)
+          // advisorStore.setAdvisor(advisor)
 
           // studentStore.getStudentById(id)
-          // advisorStore.getAdvisorrById(studentStore.getStudent.find(student?.id))
+          // advisorStore.getAdvisorById(studentStore.getStudent.find(student?.id))
           // console.log(studentStore)
 
           useStudentStore().getStudentById(id)
@@ -151,28 +178,28 @@ const router = createRouter({
             // const errorUrl = error.response.config.url;
             // console.log("Error URL:", errorUrl);
             // const resource = errorUrl.includes('T') ? 'advisors' : 'students';
-
+    
             // console.log(`Resource '${resource}' not found.`);
             // console.log("Resource not found")
 
             const errorUrl = error.response.config.url
             console.log("Error URL:", errorUrl)
-
+      
             const resourceIdentifier = errorUrl.split('/').pop() // Get the last segment
             console.log("Resource Identifier:", resourceIdentifier)
-
+      
             const isAdvisor = resourceIdentifier.charAt(0) === 'T' // Check the first character
             console.log("Is Advisor:", isAdvisor)
-
+      
             const resource = isAdvisor ? 'advisor' : 'student'
             console.log("Resource:", resource)
-            router.push({
-              name: '404-resource',
-              params: { resource }
+              router.push({
+                name: '404-resource',
+                params: { resource }
             })
-          } else {
-            router.push({ name: 'network-error' })
-          }
+            } else {
+              router.push({ name: 'network-error'})
+            }
         }
       },
       children: [
@@ -180,7 +207,7 @@ const router = createRouter({
           path: '',
           name: 'student-detail',
           component: StudentDetail,
-        }
+        },
       ]
     },
     {
@@ -198,7 +225,7 @@ const router = createRouter({
       path: '/network-error',
       name: 'network-error',
       component: NetworkErrorView
-    }, {
+    },{
       path: '/add',
       name: 'add-person',
       component: AddPerson,
@@ -214,23 +241,41 @@ router.beforeEach(async () => {
   const advisorStore = useAdvisorStore()
   const studentStore = useStudentStore()
   const authStore = useAuthStore()
+  const announcementStore = useAnnouncementStore()
   if (advisorStore.advisors.length === 0 && authStore.userRole?.includes("ROLE_ADMIN")) {
     await advisorStore.fetchAdvisorsFromDB()
+  }
+  if (advisorStore.advisors.length === 0 && authStore.userRole?.includes("ROLE_ADVISOR")
+  && authStore.id != null) {
+    await advisorStore.fetchAdvisorById(authStore.id)
+    // console.log(advisorStore.advisors)
+  }
+  if (advisorStore.advisors.length === 0 && authStore.userRole?.includes("ROLE_STUDENT")
+  && authStore.id != null) { 
+    await advisorStore.fetchAdvisorByStudent(authStore.id)
+    console.log(advisorStore.advisors)
   }
   if (studentStore.students.length === 0 && authStore.userRole?.includes("ROLE_ADMIN")) {
     await studentStore.fetchStudentsFromDB()
     // console.log(studentStore.students)
   }
   if (studentStore.students.length === 0 && authStore.userRole?.includes("ROLE_ADVISOR")
-    && authStore.id != null) {
+  && authStore.id != null) {
     await studentStore.fetchStudentsByAdvisor(authStore.id)
     // console.log(studentStore.students)
   }
+  if (studentStore.students.length === 0 && authStore.userRole?.includes("ROLE_STUDENT")
+  && authStore.id != null) {
+    await studentStore.fetchStudentById(authStore.id)
+    // console.log(studentStore.students)
+  }
+  if (announcementStore.announcements.length === 0 ) {
+    await announcementStore.fetchAnnouncements()
+  }
+  })
 
-})
-
-router.afterEach(() => {
-  NProgress.done()
-})
+  router.afterEach(() => {
+    NProgress.done()
+  })
 
 export default router
