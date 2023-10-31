868839cfe50d7df1a4be5f66271efdb03d09992a
NameSasipha <sasipha_th@cmu.ac.th>
2023-10-31 18:08:19 +0700

    update service

--- src/services/AdvisorService.ts
+++ src/services/AdvisorService.ts
@@ -3,15 +3,22 @@ import type { AxiosResponse } from 'axios'
 import type { AdvisorItem } from '@/type'
 
 export default {
-  getAdvisorrs(perPage: number, page: number): Promise<AxiosResponse<AdvisorItem[]>> {
+  getAdvisors(perPage: number, page: number): Promise<AxiosResponse<AdvisorItem[]>> {
     // perPage is limit
     // page is number of pages
-    return apiClient.get<AdvisorItem[]>('/advisor?_limit=' + perPage + '&_page=' + page)
+    return apiClient.get<AdvisorItem[]>('/advisors?_limit=' + perPage + '&_page=' + page)
   },
   getAllAdvisors() : Promise<AxiosResponse<AdvisorItem[]>> {
     return apiClient.get<AdvisorItem[]>('/AllAdvisors')
   },
   getAdvisorById(id : string) : Promise<AxiosResponse<AdvisorItem>>{
     return apiClient.get<AdvisorItem>('/advisors/'+id.toString())
+  },
+  getAdvisorByStudent(id :string) : Promise<AxiosResponse<AdvisorItem>> {
+    return apiClient.get<AdvisorItem>('/advisorByStudent/'+id.toString())
+  },
+  getAdvisorrByKeyword(keyword: string, perPage: number, page: number): Promise<AxiosResponse<AdvisorItem[]>>{
+    return apiClient.get<AdvisorItem[]>('/advisors?filter=' + keyword + '&_limit=' + perPage + '&_page=' + page)
   }
+
 }
